6bd51451480bf25b8e329e23017d3642a14e68d3
xiangjw <303310059@qq.com>
2019-10-09 17:31:53 +0800

    hehe

--- src/com/xiangjw/O/OTest.java
+++ src/com/xiangjw/O/OTest.java
@@ -1,5 +1,8 @@
 package com.xiangjw.O;
 
+/**
+ * 各种时间复杂度的情况分析
+ * */
 public class OTest {
 	
 	//时间复杂度：O(1)
@@ -82,6 +85,46 @@ public class OTest {
 		return sum;
 	}
 	
+	//arr.length即表示n
+	//最好情况时间复杂度（数组里第一个值就是a）：O(1)
+	//最坏情况时间复杂度（数组里没有a）：O(n)
+	//平均情况时间复杂度(每种情况的循环次数*每种情况的发生概率)
+	//		a不在数组中的概率为1/2，a在数组中各个位置的概率就为1/2n
+	//		1/2n + 2 / 2n + ... + n/2n + n/2 = (3n+1)/4 = O(n)
+	public static int query(int[] arr , int a) {
+		for(int i = 0; i < arr.length ; i ++) {
+			if(arr[i] == a) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	private static int[] arr = new int[100];
+	private static int count = 0;
+	
+	//arr.length即表示n
+	//这个方法的作用是，当数组没满时直接插入到最小的空闲位置，当数组已满时就遍历出所有元素之和并清空数组，和放第一个，插入值放第二个。
+	//这个方法的特点是：大部分情况下时间复杂度都是O(1)，只有个别情况时间复杂度为O(n)；
+	//		而且有频率规律，一次O(n)后紧接着是n-1次O(1)
+	//这种情况使用平均情况来分析不合适，应该使用摊还分析法计算均摊时间复杂度。
+	//把耗时多的那次操作均摊到接下来的n-1次耗时少的操作上，均摊下来，这一组连续的操作的均摊时间复杂度就是 O(1)
+	public static void insert(int val) {
+		if(count == arr.length) {
+			int sum = 0;
+			for(int i = 0 ; i < arr.length ;i ++) {
+				sum += arr[i];
+				arr[i] = 0;
+			}
+			
+			arr[0] = sum;
+			count = 1;
+		}
+		
+		arr[count++] = val;
+	}
+	
 	public static void main(String []args) {
 		int n = 1000000;
 		int n2 = 100000;
@@ -128,5 +171,4 @@ public class OTest {
 			System.out.println("cal7---" + sum + " with times ---" + (after - before) / 1000.0f);
 		}
 	}
-
 }
