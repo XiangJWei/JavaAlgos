bf04cc918895958bcbf0e77cd82d5b0e1aab634c
admin <admin@192.168.1.4>
2019-10-14 23:45:32 +0800

    排序

--- src/com/xiangjw/sort/SortTest.java
+++ src/com/xiangjw/sort/SortTest.java
@@ -2,18 +2,23 @@ package com.xiangjw.sort;
 
 public class SortTest {
 
-	public static void sortByPop(int[] a , String m) {
-		if(a == null || a.length < 2) {
+	/**
+	 * 冒泡排序
+	 * @param a
+	 * @param m
+	 */
+	public static void sortByPop(int[] a , int length) {
+		if(a == null || length < 2) {
 			System.out.println("长度不够");
 			return;
 		}
 		
-		for(int p = 0 ; p < a.length - 1 ; p++) {
+		for(int p = 0 ; p < length - 1 ; p++) {
 			boolean isSwaped = false;
-			for(int q = p + 1 ; q < a.length ; q ++) {
-				if(a[p] > a[q]) {
-					int temp = a[p];
-					a[p] = a[q];
+			for(int q = 0 ; q < length - p - 1 ; q ++) {
+				if(a[q] > a[q + 1]) {
+					int temp = a[q + 1];
+					a[q + 1] = a[q];
 					a[q] = temp;
 					isSwaped = true;
 				}
@@ -25,11 +30,61 @@ public class SortTest {
 			}
 		}
 		
-		m = "ok";
 	}
 	
-	public static void print(int[] arr , String m) {
-		System.out.print("数组元素个数" + arr.length);
+	/**
+	 * 插入排序
+	 * @param a
+	 * @param length
+	 */
+	public static void sortByInsert(int[] a , int length) {
+		if(a == null || a.length < 2) {
+			System.out.println("长度不够");
+			return;
+		}
+		
+		for(int i = 1 ; i < length ; i ++) {
+			int temp = a[i];
+			int j = i - 1;
+			for(; j >= 0 ; j--) {
+				if(temp < a[j]) {
+					a[j + 1] = a[j];//数据移动
+				}else{
+					break;//找到对应位置，马上结束循环直接插入
+				}
+			}
+			
+			a[j + 1] = temp;//插入数据
+		}
+	}
+	
+	/**
+	 * 选择排序
+	 * @param a
+	 * @param length
+	 */
+	public static void sortBySel(int[] a , int length) {
+		if(a == null || a.length < 2) {
+			System.out.println("长度不够");
+			return;
+		}
+		
+		for(int i = 0 ; i < length - 1 ; i ++) {
+			int minVal = i;
+			for(int j = i + 1 ;  j < length ; j ++) {
+				if(a[j] < a[minVal]) {
+					minVal = j;
+				}
+			}
+			
+			int temp = a[i];
+			a[i] = a[minVal];
+			a[minVal] = temp;
+		}
+	}
+	
+	public static void print(int[] arr , int length) {
+		System.out.print("数组占用空间:" + arr.length + "，数组元素个数" + length);
 		StringBuffer buffer = new StringBuffer("，[");
 		for(int i = 0 ; i < arr.length ; i ++) {
 			if(i == 0) {
@@ -38,16 +93,24 @@ public class SortTest {
 				buffer.append(",").append(arr[i]);
 			}
 		}
-		buffer.append("]").append(m);
+		buffer.append("]");
 		
 		System.out.println(buffer.toString());
 	}
 	
 	public static void main(String[] args) {
 		int[] a = {1, 2, 7 , 3, 5, 2, 10};
-		String m = "init";
-		print(a , m);
-		sortByPop(a , m);
-		print(a , m);
+		int length = 7;
+		print(a , length);
+		sortByPop(a , length);
+		print(a , length);
+		
+		int[] a1 = {1, 2, 7 , 3, 5, 2, 10};
+		sortByInsert(a1 , length);
+		print(a1 , length);
+		
+		int[] a2 = {1, 2, 7 , 3, 5, 2, 10};
+		sortBySel(a2 , length);
+		print(a2 , length);
 	}
 }
