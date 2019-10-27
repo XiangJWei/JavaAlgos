6c92b01dda2add9552521db43cf4d7659fc7f216
admin <admin@192.168.1.4>
2019-10-27 10:22:48 +0800

    完成跳表

--- src/com/xiangjw/data_structure/linkedlist/SkipLinkedList.java
+++ src/com/xiangjw/data_structure/linkedlist/SkipLinkedList.java
@@ -2,28 +2,34 @@ package com.xiangjw.data_structure.linkedlist;
 
 /**
  * 跳表，又称跳跃链表
+ * 基础链表是有序的，上层增加N层索引，每次增加删除都确保有序，这样查找时通过层级索引来跳跃式遍历链表，实现快速查找，类似基于链表实现的一种类似“二分”的算法
  * 单链表中存储有序数据，然后多了n层索引，这样便于快速查找
  * 查找、增删效率都还比较高。
  * 
- * 尚未完成，运行会报错
+ * 本例中链表是从小到大排列，考虑了重复值情况
  * @author xiangjw
  *
  */
 public class SkipLinkedList {
 	
 	private Node firstNode;//存储单链表
-	private int length;
-	private int levelCount;
+	private int length;//横向节点数量
+	private int levelCount;//整个跳表中的最大层数
 	
 	private static final float SKIPLIST_P = 0.5f;
 	private static final int MAX_LEVEL = 16;
 	
 	public SkipLinkedList() {
 		this.firstNode = new Node(-1 , 0);//哨兵
+		this.firstNode.forwards = new Node[MAX_LEVEL];
 		this.length = 0;
 		this.levelCount = 1;
 	}
 	
+	/**
+	 * 插入一个节点
+	 * @param val
+	 */
 	public void insert(int val) {
 		if(val < 0){
 			return;
@@ -31,75 +37,114 @@ public class SkipLinkedList {
 		int level = randomLevel();
 		
 		Node p = firstNode;
-		Node item = new Node(val , level);
-		//先补充第一个节点
-		for(int i = p.currLevel ; i < level ; i ++) {
-			if(p.next == null) {
-				p.next = new Node[level];
-				p.next[i] = new Node(-1 , level);
-			}else if(p.next[i] == null){
-				p.next[i] = new Node(p.next[0].data , level);
-			}else {
-				p.next[i].data = p.next[0].data;
+		Node node = new Node(val , level);//要插入的节点
+		Node[] brothers = new Node[level];//找到每一层这个待插节点的前节点。
+		for(int i = level - 1 ; i >= 0 ; i --) {
+			while(p.forwards[i] != null && p.forwards[i].data < val) {
+				p = p.forwards[i];//先找到第i层最后一个小于val的节点，也就是新节点会插到它后面
 			}
 			
-			p.next[i].currLevel = level;
+			brothers[i] = p;//赋值每一层的前节点
+			//这里两层循环中的p无需重置，因为对同一节点来讲，i层已经比较过第i层p.data<val，那么第i-1层p.data也一定小于val。所以i-1层可以继续往p.forwards往后找i-1层的合理位置
 		}
 		
-		for(int i = 0; i < level ; i ++) {
-			Node m = firstNode;
-			while(m.next != null) {
-				if(m.data < val && (m.next[i].data >= val || m.next[i] == null)) {
-					break;
-				}
-				m = m.next[i];
-			}
-			
-			item.next[i] = m.next[i];
-			m.next[i] = item;
+		//根据找到的前节点数据，我们来插入node吧
+		for(int i = 0 ; i < level ; i ++) {
+			node.forwards[i] = brothers[i].forwards[i];
+			brothers[i].forwards[i] = node;
 		}
 		
 		if(levelCount < level) {
 			levelCount = level;
 		}
-		
 		length++;
 	}
 	
+	/**
+	 * 删除一个节点
+	 * @param val
+	 */
 	public void delete(int val) {
+		if(val < 0){
+			return;
+		}
 		
+		Node p = firstNode;
+		for(int i = levelCount - 1 ; i >= 0 ; i --) {
+			while(p.forwards[i] != null && p.forwards[i].data < val) {
+				p = p.forwards[i];//先找到最后一个小于val的节点
+			}
+			
+			while(p.forwards[i] != null && p.forwards[i].data == val) {
+				p.forwards[i] = p.forwards[i].forwards[i];//然后循环判断依次删除值相等的下一跳节点
+			}
+		}
+		
+		//删除之后还要判断索引层数有没有降低
+		int delCount = 0;
+		for(int i = levelCount - 1 ; i >= 0 ; i --) {
+			if(firstNode.forwards[i] == null) {
+				delCount ++;
+			}else {
+				break;
+			}
+		}
+		levelCount -= delCount;
+		length --;
 	}
 	
-	public int get() {
+	/**
+	 * 查找节点
+	 * @param val
+	 * @return
+	 */
+	public Node find(int val) {
+		Node p = firstNode;
+		//这里看似有两个循环，但是p节点在每一层是不用从头遍历的，可以跳跃式遍历，查找的时间复杂度O(logN)，和二分查找一样
+		for(int i = levelCount - 1 ; i >= 0 ; i --) {
+			while(p.forwards[i] != null && p.forwards[i].data < val) {
+				p = p.forwards[i];//先找到最后一个小于val的节点
+			}
+			
+			if(p.forwards[i] != null && p.forwards[i].data == val) {
+				return p.forwards[i];
+			}
+		}
 		
-		return -1;
+		return null;
 	}
 	
 	private int randomLevel() {
 		int level = 1;
 		
 		while(Math.random() < SKIPLIST_P && level <=MAX_LEVEL) {
-			level ++;
+			level ++;//这里为了使level随机概率比较均匀，为1的概率是50%，为2的概率是25%，为3的概率是12.5%，以此类推
 		}
 		
 		return level;
 	}
 	
 	public void print() {
-		Node p = firstNode;
-		StringBuffer buffer = new StringBuffer();
-		while(p.next != null && p.next[0] != null) {
-			buffer.append(p.next[0].data).append(",").append(p.next[0].currLevel).append(";");
-			p = p.next[0];
+		for(int i = levelCount - 1 ; i >= 0 ; i --) {
+			Node p = firstNode;
+			System.out.print("第" + i + "层：");
+			int length = 0;
+			while(p.forwards[i] != null) {
+				System.out.print(p.forwards[i].data + ",");
+				length++;
+				p = p.forwards[i];
+			}
+			
+			System.out.println("节点数：" + length);
 		}
 		
-		System.out.println("当前链表：" + buffer.toString());
+		System.out.println("最大层数：" + levelCount + "\n");
 	}
 	
 	private class Node {
 		private int data;
-		private Node[] next;//next[0]存放原本链表的下一个，next[1]存放跳表上一层的对应对象，以此类推。
-		private int currLevel;//当前节点共有多少层
+		private Node[] forwards;//是该节点在各个level索引的下一个数据节点
+		private int currLevel;//当前节点索引层数，最底下基础链表也算一层,所以最小为1
 		public Node() {
 			super();
 		}
@@ -107,6 +152,7 @@ public class SkipLinkedList {
 			super();
 			this.data = data;
 			this.currLevel = currLevel;
+			forwards = new Node[currLevel];
 		}
 	}
 	
@@ -117,5 +163,13 @@ public class SkipLinkedList {
 		list.insert(1);list.print();
 		list.insert(2);list.print();
 		list.insert(5);list.print();
+		
+		Node node = list.find(5);
+		System.out.println("查找节点5，结果为：" + (node == null ? "木有找到" : node.data));
+		Node node2 = list.find(8);
+		System.out.println("查找节点8，结果为：" + (node2 == null ? "木有找到" : node2.data) + "\n");
+		
+		list.delete(5);list.print();
+		list.delete(2);list.print();
 	}
 }
