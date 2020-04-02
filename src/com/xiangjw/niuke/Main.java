e64d796f5173a1af9a5294a9b260cca8281c41f4
XiangJWei <303310059@qq.com>
2020-04-02 18:00:11 +0800

    a

---
+++ src/com/xiangjw/niuke/Main.java
@@ -0,0 +1,17 @@
+package com.xiangjw.niuke;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        calculate(input);
+        
+        s.close();
+    }
+    
+    public static void calculate(String info){
+    	
+    	
+    }
+}
